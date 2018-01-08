82decaa5503e2621317f99e299e8de21cf247cd5
Steven Nemetz <snemetz@wiser.com>
2018-01-08 12:58:16 -0800

    Update for CircleCI build number

--- integration-circleci-to-jenkins.sh
+++ integration-circleci-to-jenkins.sh
@@ -2,10 +2,18 @@
 #
 # Trigger Jenkins job via AWS API gateway from CircleCi
 #
-# Uses CircleCI variables and variables from a CirclCI context
+# Uses CircleCI variables and variables from a CircleCI context
 # Variables defined in context:
 #   JENKINS_API_KEY, JENKINS_URL, JENKINS_JOB_TOKEN,
 #   JENKINS_BUILD_CAUSE
+# CircleCI Doc:
+#   https://circleci.com/docs/2.0/env-vars/
+#   https://circleci.com/docs/1.0/environment-variables/
+
+# TODO:
+#   Add git ref (CIRCLE_SHA1, CIRCLE_TAG), build number (CIRCLE_BUILD_NUM, CIRCLE_PREVIOUS_BUILD_NUM)
+#   CIRCLE_STAGE job that is running
+#   CircleCI can transfer data between jobs with workspaces
 namespace=$1
 
 if [ "${CIRCLECI}" != 'true' ]; then
@@ -13,8 +21,8 @@ if [ "${CIRCLECI}" != 'true' ]; then
   exit 1
 fi
 if [ "${CIRCLE_BRANCH}" == "master" ]; then
-  # TODO: Pass commit hash CIRCLE_SHA1
   repoBaseURL="${CIRCLE_REPOSITORY_URL%.*}"
+  # Why not CIRCLE_PROJECT_REPONAME ?
   repo="${repoBaseURL##*/}"
   job_name="${namespace}+${repo}+CI+Package_Docker"
   build_cause="${JENKINS_BUILD_CAUSE}%20by%20${CIRCLE_USERNAME}"
